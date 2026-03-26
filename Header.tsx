85871ce451b5963cb24bab6da616d35c4367f34c
AlAfiz <zakariyaumuk@gmail.com>
2026-03-26 17:28:17 +0100

    feat: integrate multi-wallet connector (#8)

---
+++ Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Box } from '@mui/material';
+import ConnectWalletButton from '../wallet/ConnectWalletButton';
+import NetworkSwitcher from '../wallet/NetworkSwitcher';
+
+const Header: React.FC = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Stellar Multi-Sig Safe
+        </Typography>
+        <Box display="flex" alignItems="center" gap={3}>
+          <NetworkSwitcher />
+          <ConnectWalletButton />
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
